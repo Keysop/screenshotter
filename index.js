3ee5fa544bb121062505b8c62d78b7bb1e84922f
Sefa Said Deniz <sefasaiddeniz1@gmail.com>
2025-04-10 15:29:05 +0200

    test

--- index.js
+++ index.js
@@ -24,11 +24,10 @@ app.post("/screenshot", async (req, res) => {
       headless: true,
     });
     const page = await browser.newPage();
-    console.log("page", page);
     // Set viewport size
     await page.setViewport({ width: 1920, height: 1080 });
     // Navigate to the URL
-    await page.goto(_url);
+    await page.goto(_url, { waitUntil: "domcontentloaded" });
     // Wait for specified timeout (default 10 seconds)
     await new Promise((resolve) => setTimeout(resolve, timeout));
     console.log("page", page);
